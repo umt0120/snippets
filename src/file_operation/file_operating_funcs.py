7c30e14e69ac86465dfc9beb43ab3c354c831c5a
umt0120 <uemoto0120@gmail.com>
2021-06-13 20:51:07 +0900

    feat: 成否表現による置換用関数を追加

--- src/file_operation/file_operating_funcs.py
+++ src/file_operation/file_operating_funcs.py
@@ -1,4 +1,6 @@
 import glob
+import re
+import shutil
 
 
 def list_all_files(target_directory: str) -> list[str]:
@@ -24,3 +26,44 @@ def list_files_by_extension(target_directory: str, extension: str) -> list[str]:
         list[str]: 対象ファイルのリスト
     """
     return glob.glob(target_directory + "**/*" + extension, recursive=True)
+
+
+def replace_string_by_regexp(
+        target_directory_specified_in_glob_format: str, recursive: bool, regexp: str,
+        replacing_str: str, encoding: str = "utf-8", backup: bool = True) -> bool:
+    """globで検索したファイルに対し、正規表現で文字列置換を行う。
+
+    Args:
+        target_directory_specified_in_glob_format (str): 対象ディレクトリ。globの形式で指定する。
+        recursive (bool): 再帰的に検索するかどうか。
+        regexp (str): 置換対象を指定する正規表現。
+        replacing_str (str): 置換する文字列。
+        encoding (str): ファイルエンコード。Defaults to UTF-8.
+        backup (bool, optional): バックアップファイルを残すかどうか。 Defaults to True.
+
+    Returns:
+        bool: 処理成否
+    """
+    # 対象のファイル一覧を取得
+    target_files: list[str] = glob.glob(target_directory_specified_in_glob_format, recursive=recursive)
+
+    for target_file in target_files:
+
+        if backup:
+            # バックアップファイルを作成
+            backup_file = target_file + ".back"
+            shutil.copy(target_file, backup_file)
+
+        replaced_results: list[str] = []
+        with open(target_file, encoding=encoding) as file:
+            # 全行読み込み
+            lines: list[str] = file.readlines()
+    
+            for line in lines:
+                # 正規表現でマッチした文字列を置換
+                replaced_results.append(re.sub(regexp, replacing_str, line))
+
+        with open(target_file, mode="w", encoding=encoding) as file:
+            file.writelines(replaced_results)
+
+    return True
--- test/test_file_operation/test_file_operation.py
+++ test/test_file_operation/test_file_operation.py
@@ -1,7 +1,8 @@
-from src.file_operation.file_operating_funcs import list_all_files, list_files_by_extension
+from src.file_operation.file_operating_funcs import list_all_files, list_files_by_extension, replace_string_by_regexp
 import os
 import tempfile
 import filecmp
+import re
 
 
 def test_list_all_files() -> None:
@@ -25,6 +26,7 @@ def test_list_all_files() -> None:
         assert filecmp.dircmp(os.path.join(result_list[1], ""), os.path.join(sub_dir, ""))
         # ファイル名の比較
         assert filecmp.cmp(result_list[2], tmp_file)
+
 
 def test_list_files_by_extension() -> None:
     with tempfile.TemporaryDirectory() as tmp_dir:
@@ -50,3 +52,52 @@ def test_list_files_by_extension() -> None:
         # ファイル名の比較
         assert filecmp.cmp(result_list[0], tmp_file2)
         
+
+def test_replace_string_by_regexp() -> None:
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        # ディレクトリ名に末尾のスラッシュを追加
+        tmp_dir = os.path.join(tmp_dir, "")
+        # サブディレクトリ作成
+        sub_dir: str = os.path.join(tmp_dir, "sub_dir") 
+        os.mkdir(sub_dir)
+        tmp_file1: str = os.path.join(sub_dir, "tmp_file")
+        tmp_file2: str = os.path.join(sub_dir, "tmp_file.txt")
+        tmp_file3: str = os.path.join(sub_dir, "tmp_file.md")
+
+        with open(tmp_file1, mode="w", encoding="utf-8") as f:
+            f.write("abc123def456efg")
+        with open(tmp_file2, mode="w", encoding="utf-8") as f:
+            f.write("abc123def456efg")
+        with open(tmp_file3, mode="w", encoding="utf-8") as f:
+            f.write("abc123def456efg")
+        
+        result: bool = replace_string_by_regexp(
+            tmp_dir + "**/*.txt", recursive=True, regexp=r"[a-z]+", replacing_str="0", encoding="utf-8", backup=True)
+
+        # 処理成否
+        assert result is True
+
+        # バックアップファイルが存在すること
+        result_list: list[str] = list_all_files(tmp_dir)
+        assert len(list(filter(lambda str: re.findall(r"tmp_file.txt.back", str), result_list))) > 0
+
+        with open(tmp_file1, mode="r", encoding="utf-8") as f:
+            file_content: str = f.read()
+            raw_str: list[str] = re.findall(r"abc123def456efg", file_content)
+            replaced_str: list[str] = re.findall(r"012304560", file_content)
+            assert "".join(map(str, raw_str)) == "abc123def456efg"
+            assert len(replaced_str) == 0
+                
+        with open(tmp_file2, mode="r", encoding="utf-8") as f:
+            file_content: str = f.read()
+            raw_str: list[str] = re.findall(r"abc123def456efg", file_content)
+            replaced_str: list[str] = re.findall(r"012304560", file_content)
+            assert len(raw_str) == 0
+            assert "".join(map(str, replaced_str)) == "012304560"
+
+        with open(tmp_file3, mode="r", encoding="utf-8") as f:
+            file_content: str = f.read()
+            raw_str: list[str] = re.findall(r"abc123def456efg", file_content)
+            replaced_str: list[str] = re.findall(r"012304560", file_content)
+            assert "".join(map(str, raw_str)) == "abc123def456efg"
+            assert len(replaced_str) == 0
